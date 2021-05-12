188f18e5d906d5f509ab80131b3d4c1095f43402
Joe Burkhart <jburky15@gmail.com>
2021-05-12 15:57:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -177,16 +177,16 @@ var checkWord = function() {
             addTextToResults("<p class='userEnteredText'>> " + textInputValue + "</p>"); 
              if (textInputValueLowerCase.substr(0,5) == "open ") {                                              //if the first 5 characters = open + space 
                 openLinkInNewWindow('http://' + textInputValueLowerCase.substr(5)); 
-                addTextToResults("<i>The URL " + "<b>" + textInputValue.substr(5) + "</b>" + " should be opened now.</i>"); 
+                addTextToResults("<i>Your search for " + "<b>" + textInputValue.substr(5) + "</b>" + " should be opened in a new tab.</i>"); 
             } else if (textInputValueLowerCase.substr(0,8) == "youtube ") { 
                 openLinkInNewWindow('https://www.youtube.com/results?search_query=' + textInputValueLowerCase.substr(8)); 
-                addTextToResults("<i>I've searched on YouTube for " + "<b>" + textInputValue.substr(8) + "</b>" + " it should be opened now.</i>"); 
+                addTextToResults("<i>I headed over to YouTube and found " + "<b>" + textInputValue.substr(8) + "</b>" + " it should be opened in a new tab.</i>"); 
             } else if (textInputValueLowerCase.substr(0,7) == "google ") { 
                 openLinkInNewWindow('https://www.google.com/search?q=' + textInputValueLowerCase.substr(7)); 
-                addTextToResults("<i>I've searched on Google for " + "<b>" + textInputValue.substr(7) + "</b>" + " it should be opened now.</i>"); 
+                addTextToResults("<i>I did a search on Google for " + "<b>" + textInputValue.substr(7) + "</b>" + " it should be opened in a new tab.</i>"); 
             } else if (textInputValueLowerCase.substr(0,5) == "wiki "){ 
                 openLinkInNewWindow('https://wikipedia.org/w/index.php?search=' + textInputValueLowerCase.substr(5)); 
-                addTextToResults("<i>I've searched on Wikipedia for " + "<b>" + textInputValue.substr(5) + "</b>" + " it should be opened now.</i>"); 
+                addTextToResults("<i>I looked on Wikipedia for " + "<b>" + textInputValue.substr(5) + "</b>" + " it should be opened in a new tab.</i>"); 
             } else{ 
                 textReplies(); 
             } 
